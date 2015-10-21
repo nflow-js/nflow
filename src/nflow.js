c9a528cd9c11fb042910def3c87fc796eb9452f3
Ⓩⓞⓛⓣⓐⓝ Bourne <github@zoltanb.co.uk>
2015-10-21 23:12:56 +0100

    Update nflow.js

--- src/nflow.js
+++ src/nflow.js
@@ -1,17 +1,49 @@
-(()={
+(()=>{
   const UNSET = {}
-
-  var create = (flow, name, ...data)=>{
+  var create = (parent, name, ...data)=>{
     
-    // behaviours
-    var behaviours = {}
-    behaviours.core = {}
-    behaviours.flow = {}
+    var flow = parent && parent.create.defaultConstructor||{}
+      , behaviours = parent && parent.create.defaultBehaviours||[
+          identity,
+          connection,
+          interaction
+        ]
 
-    // public API
-    flow.create = (name, ...data)=>{}
-    flow.parent = (parent=UNSET)=>{}
+    return flow
+  }
+  
+  var guid = 0
+  var defaultBehaviours = {}
+
+  defaultBehaviours.identity = (flow)=>{
+
+    var _name;
+    flow.name = (name=UNSET) => {
+      if (name===UNSET) return _name
+      return _name = name, flow
+    }
+    
+    var _guid = guid++
+    flow.guid = (...args) => {
+      if (args.length) {
+        throw new Error('Invalid Argument. Guid-s are immutable. Please use the .name() API to change the name of a flow object')
+      }
+      return _guid
+    }
+
 
   }
-  module.exports = create("flow")
+
+  defaultBehaviours.connection = (flow)=>{
+
+    var _behaviours;
+    flow.behaviours = (behaviours=UNSET) => {
+      if (behaviours===UNSET) return _behaviours
+      return _behaviours = behaviours, flow
+    }
+  }
+
+
+
+  module.exports = create(null, "flow", {})
 })()

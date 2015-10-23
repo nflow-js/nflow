f1164a5400281d02225814abb5f3973c860aba8f
Ⓩⓞⓛⓣⓐⓝ Bourne <github@zoltanb.co.uk>
2015-10-23 17:08:33 +0100

    Update nflow.js

--- src/nflow.js
+++ src/nflow.js
@@ -1,21 +1,26 @@
 (()=>{
-  const UNSET = {}
-  var create = (parent, name, ...data)=>{
-    
-    var flow = parent.create.defaultConstructor
-    
-    parent.create
-      .defaultBehaviours
+
+  var create = (defaults, name, ...data)=>{
+    var flow = defaults.constructor()
+
+    defaults
+      .behaviours
       .forEach((d)=>d(flow))
 
     return flow
   }
   
-  /**
+
+
+   /**
    *  behaviours
    */
+  var behaviours = {}
+  
+  /**
+   *  behaviours.identity
+   */
   var guid = 0
-  var defaultBehaviours = {}
   defaultBehaviours.identity = (flow)=>{
     var _name;
     flow.name = (name=UNSET) => {
@@ -30,8 +35,25 @@
       }
       return _guid
     }
+
+    flow.name.isFlow = true
   }
 
+  defaultBehaviours.stateful = (flow)=>{
+    var _data;
+    flow.data = (...data) => {
+    if (!data.length) {
+      return (_data.length<=1)
+        ? _data[0]
+        : _data
+      }
+    return _data = data, flow
+    }
+  }
+
+  /**
+   *  behaviours.connection
+   */
   defaultBehaviours.connection = (flow)=>{
     var _parent;
     flow.parent = (parent=UNSET) => {
@@ -49,7 +71,10 @@
 
   }
   
-  defaultBehaviours.interaction = (flow)=>{
+  /**
+   *  behaviours.listener
+   */
+  defaultBehaviours.listener = (flow)=>{
     var _listeners;
     flow.on = (name=UNSET, ...args) => {
       if (name==UNSET) return _listeners.concat()
@@ -57,6 +82,33 @@
         , ERRORS.invalidListener)
       
       if (!args.length) {
+        delete _listeners[name]
+        return;
+      }
+      
+      _listeners[name] = args
+        .filter(l=>!assert(typeof(l)!='function'
+          , ERRORS.invalidListener)
+        )
+    }
+  }
+  
+
+  /**
+   *  behaviours.dispatcher
+   */
+  defaultBehaviours.dispatcher = (flow)=>{
+    
+    flow.emit = (name=UNSET, ...args) => {
+      if (name==UNSET) {
+        //TODO emit current flow object
+      }
+      if (isFlow(name))
+
+      assert(typeof(name)!='string'
+        , ERRORS.invalidEventName)
+      
+      if (args.length) {
         delete _listeners[name]
         return;
       }
@@ -68,6 +120,10 @@
     }
   }
 
+
+  /**
+   *  utils
+   */
   function assert(condition, error){
     if (condition) {
       throw new Error(error)
@@ -75,11 +131,27 @@
     return condition
   }
 
+
+  /**
+   *  consts
+   */
+  const UNSET = {}
+  const DEFAULTS = {
+    proto: ()=>{},
+    constructor:[
+      behaviours.identity,
+      behaviours.stateful,
+      behaviours.connection,
+      behaviours.dispatcher,
+      behaviours.listener
+    ]
+  }
   const ERRORS = {
     invalidGuid:     'Invalid Argument. Guid-s are immutable. Please use the .name() API to change the name of a flow object',
     invalidChildren: 'Invalid Argument. Please use child.parent(parent) to re-parent flow objects',
     invalidListener: 'Invalid Arguments. Please use .on("foo", handler) to create a listener'
+    invalidEventName:'Invalid Arguments. Please use .emit("foo", payload) to emit a flow event'
   }
 
-  module.exports = create(null, "flow", {})
+  module.exports = create(DEFAULTS, "flow")
 })()
